55f60c6fec64ba8430cb215268298ca2f63d753f
Kai Hudalla <kai.hudalla@bosch.io>
2023-12-01 13:09:11 +0100

    Do not require output

--- src/transport/datamodel/utransport.rs
+++ src/transport/datamodel/utransport.rs
@@ -34,7 +34,7 @@ pub trait UTransport {
 
     /// Register a method that will be called when a message comes in on the specific topic.
     async fn register_listener<T>(&self, topic: UUri, listener: T) -> Result<String, UStatus>
-        where T: Fn(UMessage) -> Result<(), Box<dyn std::error::Error>> + Send + Sync + 'static;
+        where T: Fn(UMessage) + Send + Sync + 'static;
 
     /// Unregister a listener. Messages arriving on this topic will no longer be processed by this listener.
     async fn unregister_listener(&self, id: String) -> Result<(), UStatus>;
