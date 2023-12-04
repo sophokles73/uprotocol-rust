d6bc29c5ad728d7797823c2d7052b486395a368a
Kai Hudalla <kai.hudalla@bosch.io>
2023-12-04 13:23:20 +0100

    Remove UListener variant of register_listener

--- src/transport/datamodel/utransport.rs
+++ src/transport/datamodel/utransport.rs
@@ -16,8 +16,6 @@ use async_trait::async_trait;
 use crate::transport::datamodel::{UAttributes, UPayload, UStatus};
 use crate::uri::datamodel::{UEntity, UUri};
 
-use super::UListener;
-
 #[derive(Debug, Clone)]
 pub struct UMessage {
     pub topic: UUri,
@@ -43,12 +41,6 @@ pub trait UTransport {
     // where
     //     T: Fn(UMessage) + Send + 'static;
     async fn register_listener(&self, topic: UUri, listener: Box<dyn Fn(UMessage) + Send + 'static>) -> Result<String, UStatus>;
-
-    /// Register a method that will be called when a message comes in on the specific topic.
-    // async fn register_listener<T>(&self, topic: UUri, listener: T) -> Result<String, UStatus>
-    // where
-    //     T: Fn(UMessage) + Send + 'static;
-    async fn register_ulistener(&self, topic: UUri, listener: Box<dyn UListener + Send + 'static>) -> Result<String, UStatus>;
 
     /// Unregister a listener. Messages arriving on this topic will no longer be processed by this listener.
     async fn unregister_listener(&self, id: String) -> Result<(), UStatus>;
